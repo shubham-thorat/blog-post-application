07d79000caf7a9590175d2cfe07b1377f845d3ea
Shubham Thorat <72251812+shubham-thorat@users.noreply.github.com>
2021-05-02 20:05:42 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
     <Provider store={store}>
       <div className="App" >
         <Switch>
-          <Route path='/blog-post-application/' exact component={FrontPage} />
+          <Route path='/blog-post-application' exact component={FrontPage} />
           <Route path='/blog-post-application/fullpost' exact component={FullPost} />
           <Route path='*' exact component={Error} />
         </Switch>
